031e7dec8bd0f1dee744af2372a15edd2876b375
lingga <lingga.ismail@imaniprima.com>
2019-05-17 14:22:46 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace AngkaGanjilGenap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bil;
+            string strbil;
+            string ket;
+
+            Console.Write("Masukan bilangan = ");
+            strbil = Console.ReadLine();
+
+            while(!int.TryParse(strbil, out bil))
+            {
+                Console.WriteLine("Masukan hanya angka!");
+                Console.Write("Masukan bilangan = ");
+                strbil = Console.ReadLine();
+            }
+
+            if (bil % 2 == 0) {
+                ket = "'Genap'";
+            } else {
+                ket = "'Ganjil'";
+            }
+
+            Console.WriteLine("Bilangan yang dimasukan adalah bilangan " + ket);
+            Console.ReadKey();
+        }
+    }
+}
